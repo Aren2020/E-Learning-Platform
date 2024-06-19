b9596b63dde3c3ad19b39cdce86e621720e643d1
Aren2020 <aren.allahverdyan@gmail.com>
2024-06-19 11:05:02 +0400

    add gitignore requirements.txt and docker compose file

---
+++ educa/educa/settings/prod.py
@@ -0,0 +1,30 @@
+from .base import *
+import os
+
+DEBUG = False
+
+ADMINS = [
+    ('Aren', 'aren.allahverdyan@gmail.com'),
+]
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
+
+# Безопасность
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
